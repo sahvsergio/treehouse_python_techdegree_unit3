17d2b0d2e70bf40d85a14b1cfe1f29d0e3d3b9c4
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-28 21:29:43 -0500

    added self.get_random_phrase to start method

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -8,13 +8,13 @@ class Game:
         #method that sets the following attributes:
         self.missed=0
         self.phrases = [
-            'Go confidently in the direction of your dreams! Live the life you\'ve imagined',
+            'Go confidently in the direction of your dreams Live the life you\'ve imagined',
             'Success usually comes to those who are too busy to be looking for it.'
             'Leave nothing for tomorrow which can be done today'
-            'Whatever the mind of man can conceive and believe, it can achieve.'
-            'Don\'t find fault, find a remedy: anyone can complain.']
+            'Whatever the mind of man can conceive and believe  it can achieve.'
+            'Dont find fault, find a remedy anyone can complain.']
         self.active_phrase=None
-        self.guesses=set()
+        self.guesses=[]
 
     
     def start(self):
@@ -29,14 +29,20 @@ class Game:
 
         '''
 
+        
+        self.active_phrase=self.get_random_phrase()
+    
+        
+       
+        
         # Calls the welcome method
-        
-        
         self.welcome()
         #game loop
+
+      
         while self.missed<5:
             user_guess=self.get_guess()
-           
+     
 
     def welcome(self):
         '''
@@ -58,15 +64,29 @@ class Game:
 
 
         try: 
-            user_guess=str(input('Please enter a letter').lower())
-            #check if the guess is actually a letter, not a number  
+            user_guess=str(input('Guess a letter: ').lower())
+            #check if the guess is actually a  letter, not a number  
             if user_guess.isalpha():
+                self.guesses.append(user_guess)
                 
           
             else:
                 raise Exception('Please enter a letter not a number')
         except Exception as e:
             print(e)
+        else:
+            print(self.guesses)
+            return str(user_guess)
+
+            
+    def get_random_phrase(self):
+        
+        '''
+        Randomly retrieves one of the phrases stored in the phrases list and returns it.
+        '''
+        copied_phrases=copy.deepcopy(self.phrases)
+        self.active_phrase=random.choice(copied_phrases)
+        return self.active_phrase
 
 
    
