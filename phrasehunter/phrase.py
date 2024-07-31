5b91b78b4b231048dc2ca79bed642e604656073d
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-30 22:15:51 -0500

    added check_letter logic

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -2,34 +2,46 @@
 #create phrase class
 class Phrase:
     # create __init with phrase parameter
-    def __init__(self, phrase):
+    def __init__(self, phrase:str):
 
         # phrase: needs to be converted to lowercase
         
-        self.phrase= phrase.lower()
+        self.phrase:str= phrase.lower()
 
-    def display(self):
+    def display(self,hidden_phrase)->str:
         '''
         presents guessed letters and 
         non-guessed_ letters 
         as underscores ex: _ _ _ _'''
+        
+    
+        
+        
+        
 
-        pass
-    def check_letter(self):
+
+    def check_letter(self, user_guess:str) -> bool:
         '''
         checks to see if the letter selected 
         by the user
         matches a letter in the phrase.
+        
 
         '''
-        pass
+        if user_guess in self.phrase:
+            return True
+        else:
+            return False
 
-    def check_complete(self):
+       
+
+    def check_complete(self)-> bool:
         '''
         check_complete(): checks to
         see if the whole phrase
         has been guessed.
         '''
+        
         
       
 
