2639c24c07a446e634aa8da3921bf4adb94e2d9f
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-08-03 12:02:59 -0500

    fixed issue related to showing guesses

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,7 +1,8 @@
-# Create your Game class logic in here.
-#create game class
+#  Create your Game class logic in here.
+# create game class
 import copy
 import random
+from typing import List
 
 
 from .phrase import Phrase
@@ -10,21 +11,18 @@ from .phrase import Phrase
 class Game:
     # The class should include an initializer or def __init__ method t
     def __init__(self):
-        #method that sets the following attributes:
-        self.missed=0
-        self.phrases = [
-            'Go confidently in the direction of your dreams Live the life you\'ve imagined',
-            'Success usually comes to those who are too busy to be looking for it.'
-            'Leave nothing for tomorrow which can be done today'
+        # method that sets the following attributes:
+        self.missed: int = 0
+        self.phrases: List[str] = [
+            'Go confidently in the direction of your dreams Live the life you have imagined',
+            'Success usually comes to those who are too busy to be looking for it.',
+            'Leave nothing for tomorrow which can be done today',
             'Whatever the mind of man can conceive and believe  it can achieve.'
             'Dont find fault, find a remedy anyone can complain.']
-        self.active_phrase=None
-        self.guesses=[]
+        self.active_phrase = None
+        self.guesses: List[str] = []
 
-    
     def start(self):
-       
-        
         '''
         -Calls the welcome method,
         -Creates the game loop,
@@ -34,75 +32,62 @@ class Game:
         -Calls the game_over method.
 
         '''
-
-
-        self.active_phrase=self.get_random_phrase()
-
-        
+        self.active_phrase = self.get_random_phrase()
+        print(self.active_phrase)
         phrase = Phrase(self.active_phrase)
-        
-
-
-
         # Calls the welcome method
         self.welcome()
-        hidden_phrase=''
-        #game loop
-        for  letter in self.active_phrase.lower():
+        hidden_phrase = ''
+        # game loop
+        for letter in self.active_phrase.lower():
             if letter.isalpha():
-                hidden_phrase+='_'
+                hidden_phrase += '_'
             elif letter.isspace():
-                hidden_phrase+=' '
+                hidden_phrase += ' '
         print(hidden_phrase)
-        
-
         print()
         print()
-
-        while self.missed<5:
+        while self.missed < 5:
+            user_guess = self.get_guess()
             
-            user_guess=self.get_guess()
-            print(type(user_guess))
             try:
-                checker=phrase.check_letter(user_guess)
-                
-                #if user_guess in self.active_phrase by using check_letter function:
-                if checker:
-                    print(checker)
-                    
-                    
-                    
-                    
+                if len(user_guess)==1:
+                    self.guesses.append(user_guess)
                 elif len(user_guess)>1:
-                   raise Exception('No more than 1 letter, please')
-                   continue
-                else:
-                    print(hidden_phrase)
-                    
-                    self.missed += 1
-                    print(self.missed)
-                    
-                    
-
-            except TypeError:
-                continue
-            except Exception as e:             
+                    raise Exception('No more than one unit please')
+                
+            except Exception as e:
                 print(f'{e}')
                 continue
-            
+            except TypeError:
+                break    
+            checker = phrase.check_letter(user_guess)
+                # if user_guess in self.active_phrase
+                # by using check_letter function:
+            if checker:
+                print('Hello, you got it ')
+                print(self.guesses)
                 
-            print(self.guesses)
+                
+            else:
+                print('Hello, you missed it ')
+                
+                self.missed+=1
+                
+                print( self.missed)
+                continue
+                print(self.guesses)
+
+
         else:
             self.game_over()
-        
 
-                
     def welcome(self):
         '''
         prints a friendly welcome message to the user
         at the start of the game
         '''
-        welcome_message='''
+        welcome_message = '''
         Welcome to the phrase hunter game.
 
         You will be presented with a hidden  phrase and your job
@@ -112,45 +97,39 @@ class Game:
         print(welcome_message)
         return None
 
+    def get_guess(self) -> str:
 
-    def get_guess(self):
-
-
-        try: 
-            user_guess=str(input('Guess a letter: ').lower())
-            #check if the guess is actually a  letter, not a number  
+        try:
+            user_guess = str(input('Guess a letter: ').lower())
+            # check if the guess is actually a  letter, not a number
             if user_guess.isalpha():
-                self.guesses.append(user_guess)
-            elif len(user_guess)>1:
-                raise Exception('Please enter only 1 letter')
+                if len(user_guess) == 1:
+                    print('Great Job, remember that it is only 1 letter at a time')
+                    
+                          
+
             else:
-
-
+               
                 raise Exception('Please enter a letter not a number')
         except Exception as e:
-            print(e)
+            print(f'{e}')
         else:
-            
-            return str(user_guess)
+            return user_guess
 
-            
     def get_random_phrase(self):
-        
+
         '''
-        Randomly retrieves one of the phrases stored in the phrases list and returns it.
+        Randomly retrieves one of
+        the phrases stored in the phrases
+        list and returns it.
         '''
-        copied_phrases=copy.deepcopy(self.phrases)
-        self.active_phrase=random.choice(copied_phrases)
+        copied_phrases = copy.deepcopy(self.phrases)
+        self.active_phrase = random.choice(copied_phrases)
         return self.active_phrase
 
     def game_over(self):
         '''
-        displays a friendly win or loss 
+        displays a friendly win or loss
         message and ends the game.
         '''
-
-
         print('Game Over')
-
-
-   
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,49 +1,46 @@
 # Create your Phrase class logic here.
-#create phrase class
+# create phrase class
 class Phrase:
     # create __init with phrase parameter
-    def __init__(self, phrase:str):
+    def __init__(self, phrase: str):
 
         # phrase: needs to be converted to lowercase
-        
-        self.phrase:str= phrase.lower()
 
-    def display(self,hidden_phrase)->str:
-        '''
-        presents guessed letters and 
-        non-guessed_ letters 
-        as underscores ex: _ _ _ _'''
-        
+        self.phrase: str = phrase.lower()
     
-        
-        
-        
 
-
-    def check_letter(self, user_guess:str) -> bool:
+    def display(self, checker) -> str:
+        
         '''
-        checks to see if the letter selected 
+        presents guessed letters and
+        non-guessed_ letters
+        as underscores ex: _ _ _ _'''
+        if checker==True:
+            print('hello')
+            
+        
+    def check_letter(self, user_guess: str) -> bool:
+        '''
+        checks to see if the letter selected
         by the user
         matches a letter in the phrase.
-        
+
 
         '''
-        if user_guess in self.phrase:
-            return True
+
+        if user_guess.lower() in self.phrase:
+            if len(user_guess) > 1:
+                return True
+            else:
+               return True
         else:
-            return False
+                return False
 
-       
 
-    def check_complete(self)-> bool:
+    def check_complete(self) -> bool:
         '''
         check_complete(): checks to
         see if the whole phrase
         has been guessed.
         '''
-        
-        
-      
-
-
-
+        pass
