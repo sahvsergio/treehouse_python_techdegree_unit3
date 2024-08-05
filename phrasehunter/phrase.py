c34d20564e9dc4ee48c7c167bf6326ffce231fc5
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-08-05 13:41:21 -0500

    united 1 letter , but 1 at the time, not multiple letters

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -15,22 +15,56 @@ class Phrase:
         presents guessed letters and
         non-guessed_ letters
         as underscores ex: _ _ _ _'''
-        #if the  user_guess is self.phrase
+        #a list to store the indexes
+        # of user_guess in self.phrase  
+        indexes=[]
+        correct_guesses=[]
+
+        #letters
+        adapted_phrase_list=[]
+        #if checker is ment
         if checker:
-            pass
-               
+            #use enumerate to get indexes
+            for index, letter in enumerate(self.phrase):
+                #check letter and user_guess are equal
+                if letter ==user_guess:   
+                    
+                    #append the index into the indexes list
+                    indexes.append(index)
+                    correct_guesses.append(user_guess)
+                
          
-           
-         
-               
-           
+                
+            print(indexes)
+            print(len(indexes))
+            recovered_phrase=''
+            for index,  letter in enumerate(hidden_phrase):
+                if index in indexes:
+                    letter=hidden_phrase[index].replace(letter, user_guess)
+                    adapted_phrase_list.append(letter)
+                    
+                   
+                else:
+                    print(letter)
+                    adapted_phrase_list.append(letter)
+                print(adapted_phrase_list)
+                print(type(adapted_phrase_list))
+                save_option=''.join(adapted_phrase_list)
+                print(save_option)
+                #adapted_phrase_list
+                
             
-           
-             
-           
-           
+                    
+                
             
-        
+            
+       
+      
+            
+            
+                
+                
+
     def check_letter(self, user_guess: str) -> bool:
         '''
         checks to see if the letter selected
